4336fb1d077a9493f4ff270cdffef5a61bf27ce6
wubuchako <56101474+wubuchako@users.noreply.github.com>
2019-12-08 22:25:13 +0900

    20191208practice

---
+++ 20191208JSpractice.js
@@ -0,0 +1,106 @@
+//ES10
+const entries = ['bob', 'sally', ... 'cindy']
+entries.flat();
+
+.flatMap
+
+const userEmail1 = "...eddytheeagle@gmail.com";
+const userEmail2 = "jonnydangerous@gmail.com...";
+
+console.log(userEmail1.trimStart());
+console.log(userEmail2.trimEnd());
+
+userPlofiles = [['commanderTom',  ], ['derekSlander',40], ['denk', 40], ['hanse',35]]
+
+const obj = Object.fromEntries(userProfiles)
+Object.entries()
+
+user Plofiles = {['commandTom', 23], ['zerek', 40], ['has',18]}
+const obj = object.fromEntries(userProfiles);
+Object.entries(obj);
+
+var myName ='Beau'
+myName = 8
+let ourName = 'camp'
+const number = 3.14
+
+var myHouse = 'Hyuga'
+myHouse = 'another'
+
+var myPet = 'cat'
+myPet = 'dog'
+
+var a;
+var b = 2;
+a= 7;
+b=a;
+console.log(a); //7
+
+var a = 5;
+var b = 10;
+var c = 'I am a';
+
+a = a + 1; //6
+b = b + 5; //15
+c = c + 'string'; //I am a string
+
+var sum = 10 + 10;
+console.log(sum);
+
+var myVar = 11;
+myVar++;
+
+var myVar = 11;
+myVar--;
+
+var ourDecimal = 5.7;
+var product = 2.0 * 2.5;
+console.log(product);
+var number = 4.4 / 2.0;
+console.log(number);
+
+var remainder;
+remainder = 11 % 3;
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+a = a + 12;   a +=12;
+b = 9 + b;    b += 9;
+c = c + 7;    c += 7;
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+a = a-6;   a -= 6;
+b = b-13;  b -= 13;
+c = c -1;  c -= 1;
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+a *=5;
+b *=3;
+c *=2;
+
+var a = 3;
+var b = 3;
+var c = 6;
+a = a / 12;
+b = b / 4;
+c = c / 7;
+
+var firstName = "alan";
+var lastName = "turing";
+var myFirstName = "beau";
+var myLastName = "carne";
+
+var myStr = 'I am a "double quoted" ';
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+var ourStr = "I come first. " + "I come second.";
+var myStr;
+
